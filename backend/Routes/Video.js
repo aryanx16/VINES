73c186c3cb1e45ef5cc5765dffd96fd2aa59e27e
Aryan Babare <babreab17@gmail.com>
2024-10-20 13:32:45 +0530

    added endpoint for update video details

--- backend/Routes/Video.js
+++ backend/Routes/Video.js
@@ -29,6 +29,7 @@ VideoRouter.post("/",CheckAuth,async(req,res)=>{
             Description:req.body.Description,
             VideoUrl:UploadedVideo.secure_url,
             VideoId:UploadedVideo.public_id,
+            Tags:req.body.Tags.split(","),
             ThumbailUrl:UploadedThumbnail.secure_url,
             ThumbailId:UploadedThumbnail.public_id,
             Category:req.body.Category,
@@ -43,8 +44,49 @@ VideoRouter.post("/",CheckAuth,async(req,res)=>{
         return res.status(400).json({message:"ERROR IN UPLOADING VIDEO..."})
     }
 })
-VideoRouter.put("/",(req,res)=>{
-    console.log("/upload working perfectly...")
+VideoRouter.put("/:VideoId",async(req,res)=>{
+    try{
+        const VideoId = req.params.VideoId;
+        const token = req.headers.authorization.split(" ")[1]
+        const user = jwt.verify(token,process.env.jwtSecret)
+        const video = await Video.findById({_id:VideoId})
+        if(!video){
+            return res.json({message:"Video not found"})
+        }
+        console.log(video)
+        if(user._id!=video.UserId){
+            console.log("USER DOSENT HAVE ACCESS TO EDIT THIS VIDEO")
+            return res.status(401).json({message:"You dont have acccess to edit this video"})
+        }
+        if(req.files){
+            await cloudinary.uploader.destroy(video.ThumbailId)
+            const UpdatedThumbnail = await cloudinary.uploader.upload(req.files.Thumbnail.tempFilePath)
+            const UpdatedData = {
+                Title:req.body.Title,
+                Description:req.body.Description,
+                Category:req.body.Category,
+                Tags:req.body.Tags.split(","),
+                ThumbailId:UpdatedThumbnail.public_id,
+                ThumbailUrl:UpdatedThumbnail.secure_url,  
+            }
+            console.log(UpdatedData)
+            const UpdatedVideo = await Video.findByIdAndUpdate(VideoId,UpdatedData,{new:true})
+            console.log(UpdatedVideo)
+        }else{
+            const UpdatedData = {
+                Title:req.body.Title,
+                Description:req.body.Description,
+                Category:req.body.Category,
+                Tags:req.body.Tags.split(","),
+            }
+            const UpdatedVideo = await Video.findByIdAndUpdate(req.params.VideoId,UpdatedData)
+            console.log(UpdatedVideo)
+        }
+        console.log("/upload working perfectly...")
+    }catch(e){
+        console.log("ERROR IN UPDATING VIDEO..."+e)
+        return res.status(401).json({message:"Token invalid"})
+    }
 })
 VideoRouter.delete("/",(req,res)=>{
     console.log("/upload working perfectly...")
